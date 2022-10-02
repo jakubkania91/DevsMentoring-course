60d64439589b32991f14b02550ce129192289130
jakubkania91 <jakub.kania91@interia.pl>
2022-10-02 18:35:13 +0200

    MyTrials

---
+++ src/TrialClassesAndMethods.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class TrialClassesAndMethods {
+    String name;
+    LocalDate deadline;
+    int priority;
+
+    TrialClassesAndMethods(String name, LocalDate deadline, int priority) {
+        this.name = name;
+        this.deadline = deadline;
+        this.priority = priority;
+
+
+    }
+}
